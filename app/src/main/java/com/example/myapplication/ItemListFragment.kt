8d738796e5395abd0b34e42fe0cfe0fbbf6754de
Bradley Ouellette <bradou@ouellettetech.com>
2023-02-10 22:30:21 -0800

    Adding basic Networking...

--- app/src/main/java/com/example/myapplication/ItemListFragment.kt
+++ app/src/main/java/com/example/myapplication/ItemListFragment.kt
@@ -1,22 +1,20 @@
 package com.example.myapplication
 
-import android.content.ClipData
-import android.content.ClipDescription
-import android.os.Build
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.placeholder.PlaceholderContent;
 import com.example.myapplication.databinding.FragmentItemListBinding
-import com.example.myapplication.databinding.ItemListContentBinding
+import java.net.HttpURLConnection
+import java.net.URI
+import java.net.URL
+import kotlin.concurrent.thread
 
 /**
  * A Fragment representing a list of Pings. This fragment
@@ -85,106 +83,65 @@ class ItemListFragment : Fragment() {
         setupRecyclerView(recyclerView, itemDetailFragmentContainer)
     }
 
+    private fun sendGet() {
+        Thread(Runnable {
+            val url = URL("https://data.cityofnewyork.us/resource/s3k6-pzi2.json?\$select=school_name,dbn")
+
+            val json = try {
+                url.readText()
+            } catch (e: Exception) {
+                "[]"
+            }
+            println(json)
+//            with(url.openConnection() as HttpURLConnection) {
+//                requestMethod = "GET"  // optional default is GET
+//
+//
+//                inputStream.bufferedReader().use {
+//                    it.lines().forEach { line ->
+//                        println(line)
+//                    }
+//                }
+//            }
+
+
+        }).start()
+    }
+
     private fun setupRecyclerView(
         recyclerView: RecyclerView,
         itemDetailFragmentContainer: View?
     ) {
+        println("BRAD NEW World 2")
+        sendGet()
 
+//        thread {
+//            val json = try {
+//                URL("https://data.cityofnewyork.us/resource/s3k6-pzi2.json").readText()
+//            } catch (e: Exception) {
+//                return@thread
+//            }
+//            println("BRAD Printing results.")
+//            println(json)
+//            println("BRAD Printed Results")
+//            //runOnUiThread { println(json) }
+//        }
+
+//        val client = HttpClient.newBuilder().build();
+//        val request = HttpRequest.newBuilder()
+//            .uri(URI.create("https://something.com"))
+//            .build();
+//        val response = client.send(request, BodyHandlers.ofString());
+//        println(response.body())
+
+
+        println("BRAD Hello World")
         recyclerView.adapter = SimpleItemRecyclerViewAdapter(
             PlaceholderContent.ITEMS, itemDetailFragmentContainer
         )
     }
 
-    class SimpleItemRecyclerViewAdapter(
-        private val values: List<PlaceholderContent.PlaceholderItem>,
-        private val itemDetailFragmentContainer: View?
-    ) :
-        RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
-            val binding =
-                ItemListContentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            return ViewHolder(binding)
-
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val item = values[position]
-            holder.idView.text = item.id
-            holder.contentView.text = item.content
-
-            with(holder.itemView) {
-                tag = item
-                setOnClickListener { itemView ->
-                    val item = itemView.tag as PlaceholderContent.PlaceholderItem
-                    val bundle = Bundle()
-                    bundle.putString(
-                        ItemDetailFragment.ARG_ITEM_ID,
-                        item.id
-                    )
-                    if (itemDetailFragmentContainer != null) {
-                        itemDetailFragmentContainer.findNavController()
-                            .navigate(R.id.fragment_item_detail, bundle)
-                    } else {
-                        itemView.findNavController().navigate(R.id.show_item_detail, bundle)
-                    }
-                }
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    /**
-                     * Context click listener to handle Right click events
-                     * from mice and trackpad input to provide a more native
-                     * experience on larger screen devices
-                     */
-                    setOnContextClickListener { v ->
-                        val item = v.tag as PlaceholderContent.PlaceholderItem
-                        Toast.makeText(
-                            v.context,
-                            "Context click of item " + item.id,
-                            Toast.LENGTH_LONG
-                        ).show()
-                        true
-                    }
-                }
-
-                setOnLongClickListener { v ->
-                    // Setting the item id as the clip data so that the drop target is able to
-                    // identify the id of the content
-                    val clipItem = ClipData.Item(item.id)
-                    val dragData = ClipData(
-                        v.tag as? CharSequence,
-                        arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
-                        clipItem
-                    )
-
-                    if (Build.VERSION.SDK_INT >= 24) {
-                        v.startDragAndDrop(
-                            dragData,
-                            View.DragShadowBuilder(v),
-                            null,
-                            0
-                        )
-                    } else {
-                        v.startDrag(
-                            dragData,
-                            View.DragShadowBuilder(v),
-                            null,
-                            0
-                        )
-                    }
-                }
-            }
-        }
-
-        override fun getItemCount() = values.size
-
-        inner class ViewHolder(binding: ItemListContentBinding) :
-            RecyclerView.ViewHolder(binding.root) {
-            val idView: TextView = binding.idText
-            val contentView: TextView = binding.content
-        }
-
-    }
 
     override fun onDestroyView() {
         super.onDestroyView()
